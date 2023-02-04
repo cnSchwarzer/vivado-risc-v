bf35e790b46451abf5f280d20fada97dd0c0b337
reito <cnschwarzer@qq.com>
2023-02-04 18:07:38 +0800

    Update

--- qemu/boot_qemu.sh
+++ qemu/boot_qemu.sh
@@ -5,11 +5,12 @@ then
     make opensbi-qemu
 fi
 
-qemu-system-riscv64 -machine virt -m 8G -nographic \
+/home/reito/qemu/build/qemu-system-riscv64 -machine virt -m 8G -nographic \
 -bios qemu/opensbi/build/platform/generic/firmware/fw_payload.bin \
 -smp cores=1 \
 -device virtio-net-device,netdev=net0 \
 -netdev user,id=net0,tftp=tftp,hostfwd=tcp::2222-:22  \
 -drive if=none,file=debian-riscv64/debian-riscv64.sd.img,format=raw,id=mydisk \
 -device ich9-ahci,id=ahci -device ide-hd,drive=mydisk,bus=ahci.0 \
--device virtio-rng-pci
+-device virtio-rng-pci \
+-cpu rv64,xreito=true
